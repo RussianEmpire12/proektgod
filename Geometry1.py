2fe2a989922dd90f06ac1d78befbcae29e49165d
RussianEmpire2 <utm120301@gmail.com>
2025-05-11 23:08:58 +0500

    Update Geometry1.py

--- Geometry1.py
+++ Geometry1.py
@@ -1,32 +1,24 @@
 # conversion_module.py
 
 def celsius_to_fahrenheit(celsius):
-    try:
-        celsius = float(celsius)
+    if isinstance(celsius, (int, float)):
         return (celsius * 9/5) + 32
-    except ValueError:
-        return None
+    return None
 
 
 def fahrenheit_to_celsius(fahrenheit):
-    try:
-        fahrenheit = float(fahrenheit)
+    if isinstance(fahrenheit, (int, float)):
         return (fahrenheit - 32) * 5/9
-    except ValueError:
-        return None
+    return None
 
 
 def hours_to_seconds(hours):
-    try:
-        hours = float(hours)
+    if isinstance(hours, (int, float)):
         return hours * 3600
-    except ValueError:
-        return None
+    return None
 
 
 def seconds_to_hours(seconds):
-    try:
-        seconds = float(seconds)
+    if isinstance(seconds, (int, float)):
         return seconds / 3600
-    except ValueError:
-        return None
+    return None
